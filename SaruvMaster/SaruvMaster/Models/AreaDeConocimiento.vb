f1f820058b4de90ae210abc2abf484f16b86296e
Ricardo Ortega <ricardo.ortega@unitec.edu>
2015-04-27 19:01:17 -0600

    Fixed css sidebar and error messages from controllers

--- SaruvMaster/SaruvMaster/Models/AreaDeConocimiento.vb
+++ SaruvMaster/SaruvMaster/Models/AreaDeConocimiento.vb
@@ -4,7 +4,7 @@
 Public Class AreaDeConocimiento
 
     Public Property ID As Integer
-    <Display(Name:="Área de conocimiento"), Required>
+    <Display(Name:="Área de conocimiento"), Required(ErrorMessage:="Este campo es obligatorio")>
     <StringLength(255, MinimumLength:=5, ErrorMessage:="Solo se puede un minimo de 5 letras y un maximo de 255 letras")>
     <RegularExpression("^([a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð]+ ?)+?$", ErrorMessage:="Solo se aceptan letras y un espacio entre palabras")>
     Public Property Nombre As String
--- SaruvMaster/SaruvMaster/Models/Empresa.vb
+++ SaruvMaster/SaruvMaster/Models/Empresa.vb
@@ -4,27 +4,27 @@ Public Class Empresa
 
     Public Property ID As Integer
 
-    <Display(Name:="Empresa"), Required>
+    <Display(Name:="Empresa"), Required(ErrorMessage:="Este campo es obligatorio")>
     <StringLength(255, MinimumLength:=5, ErrorMessage:="Solo se puede un mínimo de 5 letras y un máximo de 255 letras")>
     <RegularExpression("^([a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð]+ ?)+?$", ErrorMessage:="Solo se aceptan letras y espacios")>
     Public Property Nombre As String
 
-    <Display(Name:="Dirección"), Required>
+    <Display(Name:="Dirección"), Required(ErrorMessage:="Este campo es obligatorio")>
     <StringLength(255, MinimumLength:=5, ErrorMessage:="Solo se puede un mínimo de 5 letras y un máximo de 255 letras")>
     <RegularExpression("^([a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð\d]+ ?.?)+?$", ErrorMessage:="Solo se aceptan letras, números, punto y un espacio entre palabras")>
     Public Property Direccion As String
 
-    <Display(Name:="Teléfono"), Required>
+    <Display(Name:="Teléfono"), Required(ErrorMessage:="Este campo es obligatorio")>
     <StringLength(255, MinimumLength:=5, ErrorMessage:="Solo se puede un mínimo de 5 letras y un máximo de 255 letras")>
     <RegularExpression("^(\d{3}|\(\s*\d{3}\s*\))\s*-?\s*\d{4}\s*-?\s*\d{4}$", ErrorMessage:="Solo se aceptan los formatos: (504)-2233-4455, 504-2233-4455, 504-22334455, 50422334455")>
     Public Property Telefono As String
 
-    <Display(Name:="Ciudad"), Required>
+    <Display(Name:="Ciudad"), Required(ErrorMessage:="Este campo es obligatorio")>
     <StringLength(255, MinimumLength:=5, ErrorMessage:="Solo se puede un mínimo de 5 letras y un máximo de 255 letras")>
     <RegularExpression("^[a-zA-Z ]*$", ErrorMessage:="Solo se aceptan letras  y espacios")>
     Public Property Ciudad As String
 
-    <Display(Name:="Departamento"), Required>
+    <Display(Name:="Departamento"), Required(ErrorMessage:="Este campo es obligatorio")>
     <StringLength(255, MinimumLength:=5, ErrorMessage:="Solo se puede un mínimo de 5 letras y un máximo de 255 letras")>
     <RegularExpression("^[a-zA-Z ]*$", ErrorMessage:="Solo se aceptan letras y espacios")>
     Public Property Departamento As String
