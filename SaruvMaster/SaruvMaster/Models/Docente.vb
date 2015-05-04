1ccb44908859992e0901771025da4c2c3f7f0d70
MatamorosF3 <matamorosf3@gmail.com>
2015-05-03 19:47:51 -0600

    fixed bug Docente

--- SaruvMaster/SaruvMaster/Models/Docente.vb
+++ SaruvMaster/SaruvMaster/Models/Docente.vb
@@ -18,7 +18,10 @@ Public Class Docente
     <EmailAddress(ErrorMessage:="Asegurate de tener un formato correcto de correo")>
     Public Property correoElectronico As String
 
-    <Display(Name:="Teléfono")>
+
+    <Display(Name:="Teléfono"), Required(ErrorMessage:="Este campo es obligatorio")>
+    <StringLength(255, MinimumLength:=5, ErrorMessage:="Solo se puede un mínimo de 5 letras y un máximo de 255 letras")>
+    <RegularExpression("^(\d{3}|\(\s*\d{3}\s*\))\s*-?\s*\d{4}\s*-?\s*\d{4}$", ErrorMessage:="Solo se aceptan los formatos: (504)-2233-4455, 504-2233-4455, 504-22334455, 50422334455")>
     Public Property telefono As String
 
     <Display(Name:="Área de conocimiento")>
