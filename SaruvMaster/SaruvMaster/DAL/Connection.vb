5c192114c77f6c3e964f350e0ef90b803030331b
unknown <matamorosf3@gmail.com>
2015-06-02 01:21:12 -0600

    SoftDelete ready

--- SaruvMaster/SaruvMaster/DAL/Connection.vb
+++ SaruvMaster/SaruvMaster/DAL/Connection.vb
@@ -8,8 +8,10 @@ Public Class Connection
 
     Public Sub New()
         MyBase.New("Context")
-
     End Sub
+
+    Private Shared _mappingCache As New Dictionary(Of Type, Core.Metadata.Edm.EntitySetBase)()
+
     Public Property AreaDeConocimiento As DbSet(Of AreaDeConocimiento)
 
     Public Property Curso As DbSet(Of Curso)
@@ -28,13 +30,17 @@ Public Class Connection
 
     Public Property TipoDeRecurso As DbSet(Of TipoDeRecurso)
 
-
     Public Property Departamento As DbSet(Of Departamento)
 
     Public Property RolPorDepartamento As DbSet(Of RolPorDepartamento)
+
     Public Property Usuario As DbSet(Of Usuario)
+
     Public Property RecursoPorUsuario As DbSet(Of RecursoPorUsuario)
-    Public Property EventosEstudio As DbSet(of EventosEstudio)
+
+    Public Property EventosEstudio As DbSet(Of EventosEstudio)
+
+    Public Property Recursoes As System.Data.Entity.DbSet(Of Recurso)
 
     Protected Overrides Sub OnModelCreating(ByVal modelBuilder As DbModelBuilder)
 
@@ -71,16 +77,70 @@ Public Class Connection
     End Sub
 
     Public Overrides Function Savechanges() As Integer
+
         Dim changed = ChangeTracker.Entries()
-
         For Each item In changed.Where(Function(m) m.State.Equals(EntityState.Deleted))
-            item.State = EntityState.Unchanged
+            SoftDelete(item, "")
+        Next
 
-            Dim s = ObjectContext.GetObjecitem.GetType()
-
+        Return MyBase.SaveChanges()
+    End Function
+    Public Function deshabilitar(value As String) As Integer
+        Dim changed = ChangeTracker.Entries()
+        For Each item In changed.Where(Function(m) m.State.Equals(EntityState.Deleted))
+            SoftDelete(item, value)
         Next
         Return MyBase.SaveChanges()
     End Function
+    Private Sub SoftDelete(entry As Infrastructure.DbEntityEntry, value As String)
 
-    Public Property Recursoes As System.Data.Entity.DbSet(Of Recurso)
+        Dim entryEntityType As Type = entry.Entity.[GetType]()
+        Dim tableName As String = GetTableName(entryEntityType)
+        Dim primaryKeyName As String = GetPrimaryKeyName(entryEntityType)
+
+        Dim sql As String
+        If IsNothing(value) Then
+            sql = String.Format("UPDATE {0} SET IsDeleted = 1 WHERE {1} = @id", tableName, primaryKeyName)
+        Else
+            sql = String.Format("UPDATE {0} SET IsDeleted = 2 WHERE {1} = @id", tableName, primaryKeyName)
+        End If
+
+
+        Database.ExecuteSqlCommand(sql, New SqlClient.SqlParameter("@id", entry.OriginalValues(primaryKeyName)))
+
+        ' prevent hard delete            
+        entry.State = EntityState.Detached
+    End Sub
+
+
+    Private Function GetTableName(type As Type) As String
+            Dim es As Core.Metadata.Edm.EntitySetBase = GetEntitySet(type)
+
+            Return String.Format("[{0}].[{1}]", es.MetadataProperties("Schema").Value, es.MetadataProperties("Table").Value)
+        End Function
+
+        Private Function GetPrimaryKeyName(type As Type) As String
+            Dim es As Core.Metadata.Edm.EntitySetBase = GetEntitySet(type)
+
+            Return es.ElementType.KeyMembers(0).Name
+        End Function
+
+        Private Function GetEntitySet(type As Type) As Core.Metadata.Edm.EntitySetBase
+            If Not _mappingCache.ContainsKey(type) Then
+                Dim octx As Core.Objects.ObjectContext = DirectCast(Me, Infrastructure.IObjectContextAdapter).ObjectContext
+
+                Dim typeName As String = Core.Objects.ObjectContext.GetObjectType(type).Name
+
+            Dim es = octx.MetadataWorkspace.GetItemCollection(Core.Metadata.Edm.DataSpace.SSpace).GetItems(Of Core.Metadata.Edm.EntityContainer)().SelectMany(Function(c) c.BaseEntitySets.Where(Function(e) e.Name = typeName)).FirstOrDefault()
+
+            If es Is Nothing Then
+                    Throw New ArgumentException("Entity type not found in GetTableName", typeName)
+                End If
+
+                _mappingCache.Add(type, es)
+            End If
+
+            Return _mappingCache(type)
+        End Function
+
 End Class
